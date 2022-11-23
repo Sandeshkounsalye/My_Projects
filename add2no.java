cbba70a004af4331c4458e3a049d0f5cc1e9a648
Sandesh <sandeshpk411@gmail.com>
2022-11-23 11:49:19 +0530

    Arrry

---
+++ add2no.java
@@ -0,0 +1,11 @@
+public class add2no {
+    public static void main(String[] args) {
+       int  a=10;
+        int b=20;
+        int c=a+b;
+        System.out.print("A and b add is ");
+
+        System.out.println("Addition is:"+c);
+
+    }
+}
